7cc47cd49d2b2213ef2b8d8bfb6a5dda6c94101d
wuaikaiyuan <lsc_000@126.com>
2024-11-19 12:00:54 +0800

    添加注释；使用git-cliff记录修改日志

--- distil-whisper-api.py
+++ distil-whisper-api.py
@@ -1,11 +1,11 @@
-
 import os
 from groq import Groq
 
 parent_path = os.path.dirname("I:/人生七年/")
 
+# 使用GROQ API进行语音识别
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"),)
-filename = os.path.join(parent_path, "1.1964.mp3")
+filename = os.path.join(parent_path, "1.1964.wav")
 print(filename)
 
 with open(filename, "rb") as file:
